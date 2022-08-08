198c7125f27f4fe2c2f40168c0cc831286d282ce
DESKTOP-641VCJ4\tj-bu <726ouleesun@naver.com>
2022-08-08 18:21:01 +0900

    1day yoodareen layout

--- about_man.php
+++ about_man.php
@@ -1,3 +1,75 @@
 <?php include 'sub-header.php' ?>
-  남성향수
+<section class="ydr-sauvage">
+    <section class="ydr-sauvage-video">
+      s
+    </section>
+  </section>
+  <main class="ydr-sauvage-purfume">
+    <ul class="ydr-sauvage-purfume-img">
+      <li></li>
+      <li></li>
+    </ul>
+    <section class="ydr-sauvage-banner">
+      1
+    </section>
+  </main>
+
+
+  <section class="ydr-collection">
+    <section class="ydr-collection-water">
+      <ul class="ybr-colection-wrap">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </section>
+    <section class="ydr-collection-marble">
+      <ul class="ybr-colection-wrap">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </section>
+  </section>
+
+
+  <figure class="ydr-homme">
+    <img src="./img/91e91562d5295933c89ec3649e8045eb.jpg" alt="">
+    <figcaption class="ydr-home-text">
+      <h3>DIOR HOMME</h6>
+        <p>
+          디올 옴므는 가공되지 않은 우드와 다채로운 부드러움으로 관능적인 남성성을 새롭게 재해석합니다. <br>
+          부드러운 우드 노트의 중심에는, 남성적인 아틀라스 시더가 스파이스 향이 감도는 아이티산 베티버의 따뜻함을 감싸줍니다. 마치 다듬어 지지 않은 목재가 디올에 의해 수작업으로 정교히 조각된 듯한 느낌을
+          선사합니다. <br>
+          디올 옴므는 현대 남성의 다채로운 모습을 담아내었습니다. 남성미가 넘치는 시그니처 향뿐만 아니라 강인함과 부드러움이 교차하는 부드럽고 센슈얼한 느낌도 함께 선사합니다. <br>
+          디올 옴므 오 드 뚜왈렛은 대담한 향기를 지닌 향수로 관능적인 인상을 남깁니다. <br>
+        </p>
+    </figcaption>
+  </figure>
+
+  <nav class="ydr-homme-nav">
+    <div class="ydr-homme-name">
+      <span>
+        <a href="#">DIOR HOMME COLOGNE</a>
+        <a href="#">DIOR HOMME</a>
+        <a href="#">DIOR HOMME INTENSE</a>
+      </span>
+      <p>디올 옴므는 마치 우아하고 모던한 수트와 같이 향이 퍼지는 곳마다 우아함을 선사합니다. 디올 옴므 인텐스 오 드 퍼퓸은 극도로 장엄한 세련미를 대담하고 강렬하게 표출합니다. 감각적인 엠버 노트와 고귀한 우드 베이스로 강조된 인텐스 아이리스는 강렬한 매력을 선사합니다. 향기롭고 관능적인 향기가 끊임없이 이어집니다.</p>
+    </div>
+    <img src="./img/3348901419147_2.webp" alt="">
+  </nav>
+
+  <section class="ydr-homme-purfume">
+    <ul class="ydr-homme-ul">
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+    </ul>
+  </section>
 <?php include 'sub-footer.php' ?>
