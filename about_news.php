a125bd984f2e124b987ef405ea99849adf39e4ea
DESKTOP-ST2AIC0\tj-bu <tjdwlsakfn2@naver.com>
2022-08-08 18:22:29 +0900

    1day leeseongjin

--- about_news.php
+++ about_news.php
@@ -1,3 +1,43 @@
 <?php include 'sub-header.php' ?>
-  디올소개
+<main class="lee-main">
+<header class="lee-header">
+  <h1>
+   <a href="#">NEWS</a>
+   <a href="#">«DIOR UNTOLD» PODCASTS</a>
+  </h1>
+    <ul>
+      <li><a href="#">ALL</a></li>
+      <li><a href="#">NEWS</a></li>
+      <li><a href="#">SPECIAL OFFERS</a></li>
+    </ul>
+    </header>
+    
+    <hr class="lee-line" size="0.05px";>
+
+    <nav class="lee-container">
+      <h6>JULY2022</h6>
+        <div>
+      <div>
+        <figure>
+          <img src="./img/model/women2.png" alt="">
+        </figure>
+        <span>
+         <h3>그녀들은 Dior를 만들었습니다</h3><br>
+         <p>DIOR UNTOLD THE power of red</p>
+         <p>DIOR UNTOLD THE power of red</p>
+        </span>
+      </div>
+      <div>
+        <figure>
+          <img src="./img/model/women3.jpg" alt="">
+        </figure>
+       <span>
+         <h3>꽃이 만발한 디올</h3><br>
+         <p>In the opening episode</p>
+         <p>Dior in Bloom</p>
+       </span>
+       </div>
+      </div>
+    </nav>
+    </main>
 <?php include 'sub-footer.php' ?>
