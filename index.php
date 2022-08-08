c8b5ec2cc38dc68c020a381abb415406abcacdcd
DESKTOP-0ABCL26\tj-bu <ljm5704@gmail.com>
2022-08-08 18:24:42 +0900

    로고 glitch

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php include 'header.php' ?>
-  인덱스 메인 첫페이지
+<link rel="stylesheet" href="./css/index.css">
+  <h1 class="hidden">인덱스</h1>
+  <figure class="khkindexlogo">
+    <img src="./img/김희경/intro/logo-white.png" alt="logo-white">
+    <img src="./img/김희경/intro/logo-blue.png" alt="logo-blue">
+    <img src="./img/김희경/intro/logo-yellow.png" alt="logo-yellow">
+  </figure>
 <?php include 'footer.php' ?>
