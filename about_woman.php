7ae9ead27ded698aaeaf0bca084b6c52a13773dc
DESKTOP-SL0JOSE\tj-bu <>
2022-08-10 18:25:18 +0900

    구조 재편성 및 수정작업...정형빈 0810 06:25

--- about_woman.php
+++ about_woman.php
@@ -7,88 +7,89 @@
         <figure>
           <img src="./img/HBJ/Miss^diol/missdiol^eaude^parpum.jpg" alt="">
         </figure>
+        <div class="missdiol-textbox">
+          름답게 수 놓인 싱그러운 은방울꽃 노트와
+          살구 향이 감도는 피오니가 장미의꽃잎처럼
+        </div><!-- textbox -->
       </a>
-      <div class="missdiol-textbox1">
-        <h4>
-          <span> "올  뉴  미스디올  오  드  퍼퓸."</span>
-          <p>아름답게 수놓인 싱그러운 은방울꽃 노트와 <br>
-            살구 향이 감도는 피오니 어코드... 그리고<br>
-            파우더리한 아이리스 노트가 조화를 이룹니다.
-        </h4>
-
-        <div class="more1">more</div>
-      </div>
-    </li>
+    </li><!-- 1 -->
     <li class="missdiol-pic2">
       <a href="#">
         <figure>
           <img src="./img/HBJ/Miss^diol/missdiol^flower.jpg" alt="">
         </figure>
       </a>
-      <div class="textbox">
+      <div class="missdiol-textbox">
+        asfa adsf
       </div>
-    </li>
+    </li><!-- 2 -->
     <li class="missdiol-pic3">
       <a href="#">
         <figure>
           <img src="./img/HBJ/Miss^diol/irisflowers.jpg" alt="">
         </figure>
       </a>
-      <div class="textbox"></div>
-    </li>
+      <div class="missdiol-textbox">
+        름답게 수 놓인 싱그러운 은방울꽃 노트와
+        살구 향이 감도는 피오니가 장미의꽃잎처럼
+      </div>
+    </li><!-- 3 -->
     <li class="missdiol-pic4">
+      <div class="missdiol-textbox">
+
+        름답게 수 놓인 싱그러운 은방울꽃 노트와
+        살구 향이 감도는 피오니가 장미의꽃잎처럼
+      </div><!-- textbox -->
+    </li><!-- 4 -->
+    <li class="missdiol-pic5">
       <a href="#">
         <figure>
           <img src="./img/HBJ/Miss^diol/diol^jisoo_whitetone.jpg" alt="">
         </figure>
+        <div class="missdiol-textbox">
+
+          름답게 수 놓인 싱그러운 은방울꽃 노트와
+          살구 향이 감도는 피오니가 장미의꽃잎처럼
+        </div><!-- textbox -->
       </a>
-      <div class="textbox"></div>
     </li>
-    <li class="missdiol-pic5">
+    <li class="missdiol-pic6">
       <a href="#">
         <figure>
           <img src="./img/HBJ/Miss^diol/diol^jisoo_pinktone.jpg" alt="">
         </figure>
       </a>
-      <div class="textbox"></div>
     </li>
-    <li class="missdiol-pic6">
+
+    <li class="missdiol-pic7">
       <a href="#">
         <figure>
           <img src="./img/HBJ/Miss^diol/missdiol^eaude^toillet.jpg" alt="">
         </figure>
       </a>
-      <div class="textbox">
-        <h3>
-          <p></p>
-        </h3>
-        <div class="more2">
-
-        </div>
-      </div>
-    </li>
-    <li class="missdiol-pic7">
-      <a href="#">
-        <figure>
-          <img src="./img/HBJ/Miss^diol/rose^essance.jpg" alt="">
-        </figure>
-      </a>
-      <div class="textbox">
-        <h3>
-          <p></p>
-        </h3>
-        <div class="more3">
-
-        </div>
+      <div class="missdiol-textbox">
+        름답게 수 놓인 싱그러운 은방울꽃 노트와
+        살구 향이 감도는 피오니가 장미의꽃잎처럼
       </div>
     </li>
     <li class="missdiol-pic8">
       <a href="#">
         <figure>
+          <img src="./img/HBJ/Miss^diol/rose^essance.jpg" alt="">
+        </figure>
+      </a>
+      <div class="missdiol-textbox">
+        름답게 수 놓인 싱그러운 은방울꽃 노트와
+        살구 향이 감도는 피오니가 장미의꽃잎처럼
+        <!-- textbox -->
+      </div>
+    </li>
+    <li class="missdiol-pic9">
+      <a href="#">
+        <figure>
           <img src="./img/HBJ/Miss^diol/natali^portman.jpg" alt="">
         </figure>
       </a>
-      <div class="textbox"></div>
     </li>
 
   </ul>
