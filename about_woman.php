2315fd8583db6e1e0f35fb82b7a607e5530d6c2c
DESKTOP-SL0JOSE\tj-bu <>
2022-08-11 18:25:40 +0900

    pc화면 1000px 이상일때 구조 편성및 텍스트 까지 작업...2022-08-11 06:25 정형빈

--- about_woman.php
+++ about_woman.php
@@ -8,8 +8,14 @@
           <img src="./img/HBJ/Miss^diol/missdiol^eaude^parpum.jpg" alt="">
         </figure>
         <div class="missdiol-textbox">
-          름답게 수 놓인 싱그러운 은방울꽃 노트와
-          살구 향이 감도는 피오니가 장미의꽃잎처럼
+          <div>
+            <h2>"올 뉴 미스디올 오 드 퍼퓸"</h2> <br>
+            <p>싱그러운 은방울 꽃과 선명한 피오니가 장미의 꽃잎처럼 부드럽고 센슈얼틱한 매력을 이끌어내고 <br>
+              파우더리스한 아이리스가 향기를 한 층 더 풍성하게 피워냅니다.</p> <br>
+            <div class="more">
+              <p>more</p>
+            </div>
+          </div>
         </div><!-- textbox -->
       </a>
     </li><!-- 1 -->
@@ -20,8 +26,10 @@
         </figure>
       </a>
       <div class="missdiol-textbox">
-        asfa adsf
-      </div>
+        <div>
+          <p>"f.l.o.r.a.l"</p>
+        </div>
+      </div><!-- short textbox -->
     </li><!-- 2 -->
     <li class="missdiol-pic3">
       <a href="#">
@@ -30,23 +38,31 @@
         </figure>
       </a>
       <div class="missdiol-textbox">
-        름답게 수 놓인 싱그러운 은방울꽃 노트와
-        살구 향이 감도는 피오니가 장미의꽃잎처럼
-      </div>
-    </li><!-- 3 -->
+
+        <div>
+          름답게 수 놓인 싱그러운 은방울꽃 노트와
+          살구 향이 감도는 피오니가 장미의꽃잎처럼
+        </div>
+
+      </div><!-- long textbox -->
+    </li><!-- 3-->
     <li class="missdiol-pic4">
       <div class="missdiol-textbox">
+        <div>
 
-        름답게 수 놓인 싱그러운 은방울꽃 노트와
-        살구 향이 감도는 피오니가 장미의꽃잎처럼
-      </div><!-- textbox -->
+          <em>2022</em> <br>
+          <p> FALL INTO BEAUTIFUL SCENT <br></p>
+          www.dior.com
+
+        </div>
+      </div><!-- large textbox-->
     </li><!-- 4 -->
     <li class="missdiol-pic5">
       <a href="#">
         <figure>
           <img src="./img/HBJ/Miss^diol/diol^jisoo_whitetone.jpg" alt="">
         </figure>
-        <div class="missdiol-textbox">
+        <div class="missdiol-textbox hidden">
 
           름답게 수 놓인 싱그러운 은방울꽃 노트와
           살구 향이 감도는 피오니가 장미의꽃잎처럼
@@ -68,9 +84,17 @@
         </figure>
       </a>
       <div class="missdiol-textbox">
-        름답게 수 놓인 싱그러운 은방울꽃 노트와
-        살구 향이 감도는 피오니가 장미의꽃잎처럼
+        <div>
+          <h2>"올 뉴 미스디올 오 드 뚜왈렛"</h2> <br>
+          <p>탑노트에서 블러드 오렌지와 만다린이 눈부신 매력을 더해주며, <br>
+            미들노트에서 은방울꽃 어코드가 그라스산 로즈의 향에 가볍고 산뜻한 향기를 더해줍니다. <br>
+            베이스 노트의 선명한 패츌리 어코드가 행복으로 가득하게 만들어 줍니다.</p><br>
+          <div class="more">
+             <p>more</p> 
+            </div>
+        </div>
       </div>
+      <!-- 일반 박스 형태 텍스트박스 -->
     </li>
     <li class="missdiol-pic8">
       <a href="#">
@@ -79,9 +103,16 @@
         </figure>
       </a>
       <div class="missdiol-textbox">
-        름답게 수 놓인 싱그러운 은방울꽃 노트와
-        살구 향이 감도는 피오니가 장미의꽃잎처럼
-        <!-- textbox -->
+        <div>
+          <h2>"올 뉴 미스디올 로즈 에쌍스"</h2><br>
+          <p>그라스의 마농 농장에서 수확한 메이로즈와 싱그럽고 매혹적인 플로럴과 우디의 베티버,패츌리 그리고 가이악 향기까지... <br>
+            조화롭게 어우러진 가운데 머스크 노트가 부드럽게 감싸진 특별한 향기를 선사하는 최초의 미스 디올 빈티지 리미티드 향수입니다.</p><br>
+          <!-- 일반 박스 형태 텍스트박스 -->
+          <div class="more"> 
+            <p>more</p>
+           </div>
+        </div>
+
       </div>
     </li>
     <li class="missdiol-pic9">
