5afd467e4aa926e0d90f41443bb9b1abbc0386a3
DESKTOP-SL0JOSE\tj-bu <>
2022-08-12 18:20:07 +0900

    텍스트 마무리작업 및 parallrex  js관련밑작업 2022-08-12 18:20 정형빈

--- about_woman.php
+++ about_woman.php
@@ -1,5 +1,8 @@
 <?php include 'sub-header.php' ?>
 <link rel="stylesheet" href="./css/about-woman-kjh.css">
+<script src="./js/about-woman.js"></script>
+<script src="./js/smoothscroll.js"></script>
+<script src="./js/window.js"></script>
 <section class="about-woman-section about-woman-section1">
   <ul class="clearfix">
     <li class="missdiol-pic1">
@@ -12,9 +15,9 @@
             <h2>"올 뉴 미스디올 오 드 퍼퓸"</h2> <br>
             <p>싱그러운 은방울 꽃과 선명한 피오니가 장미의 꽃잎처럼 부드럽고 센슈얼틱한 매력을 이끌어내고 <br>
               파우더리스한 아이리스가 향기를 한 층 더 풍성하게 피워냅니다.</p> <br>
-            <div class="more">
-              <p>more</p>
-            </div>
+           <button class="more">
+            <div>more</div>
+           </button>
           </div>
         </div><!-- textbox -->
       </a>
@@ -27,7 +30,7 @@
       </a>
       <div class="missdiol-textbox">
         <div>
-          <p>"f.l.o.r.a.l"</p>
+          <p class="">"f.l.o.r.a.l"</p>
         </div>
       </div><!-- short textbox -->
     </li><!-- 2 -->
@@ -39,9 +42,11 @@
       </a>
       <div class="missdiol-textbox">
 
-        <div>
-          름답게 수 놓인 싱그러운 은방울꽃 노트와
-          살구 향이 감도는 피오니가 장미의꽃잎처럼
+        <div class="">
+          <h2>" 새로운 미스디올 컬렉션 그 한 병 안에서 내츄럴틱 한 꽃의 향연에 빠지다."</h2>  <br>
+            <p>싱그러운 은방울꽃,선명해지는 피오니,파우더리스한 아이리스,상큼한블러드 오렌지와 고귀한 만다린,베티퍼,패츌리,가이악...그리고 언제나 만나도 새로운 메이로즈까지
+              이 모든 향을  미스디올 컬렉션에 담아냈습니다. 지금 인근 매장에서 만나보세요..!
+            </p>
         </div>
 
       </div><!-- long textbox -->
@@ -50,9 +55,9 @@
       <div class="missdiol-textbox">
         <div>
 
-          <em>2022</em> <br>
+                  <h2>2022</h2> <br>
           <p> FALL INTO BEAUTIFUL SCENT <br></p>
-          www.dior.com
+            <span>www.dior.com</span> 
 
         </div>
       </div><!-- large textbox-->
@@ -62,11 +67,6 @@
         <figure>
           <img src="./img/HBJ/Miss^diol/diol^jisoo_whitetone.jpg" alt="">
         </figure>
-        <div class="missdiol-textbox hidden">
-
-          름답게 수 놓인 싱그러운 은방울꽃 노트와
-          살구 향이 감도는 피오니가 장미의꽃잎처럼
-        </div><!-- textbox -->
       </a>
     </li>
     <li class="missdiol-pic6">
@@ -86,12 +86,12 @@
       <div class="missdiol-textbox">
         <div>
           <h2>"올 뉴 미스디올 오 드 뚜왈렛"</h2> <br>
-          <p>탑노트에서 블러드 오렌지와 만다린이 눈부신 매력을 더해주며, <br>
-            미들노트에서 은방울꽃 어코드가 그라스산 로즈의 향에 가볍고 산뜻한 향기를 더해줍니다. <br>
+          <p>탑 노트에서 블러드 오렌지와 만다린이 눈부신 매력을 더해주며, <br>
+            미들 노트에서 은방울꽃 어코드가 그라스산 로즈의 향에 가볍고 산뜻한 향기를 더해줍니다. <br>
             베이스 노트의 선명한 패츌리 어코드가 행복으로 가득하게 만들어 줍니다.</p><br>
-          <div class="more">
-             <p>more</p> 
-            </div>
+            <button class="more">
+              <div>more</div>
+             </button>
         </div>
       </div>
       <!-- 일반 박스 형태 텍스트박스 -->
@@ -108,14 +108,14 @@
           <p>그라스의 마농 농장에서 수확한 메이로즈와 싱그럽고 매혹적인 플로럴과 우디의 베티버,패츌리 그리고 가이악 향기까지... <br>
             조화롭게 어우러진 가운데 머스크 노트가 부드럽게 감싸진 특별한 향기를 선사하는 최초의 미스 디올 빈티지 리미티드 향수입니다.</p><br>
           <!-- 일반 박스 형태 텍스트박스 -->
-          <div class="more"> 
-            <p>more</p>
-           </div>
+          <button class="more" >
+            <div> more </div>
+          </button>
         </div>
 
       </div>
     </li>
-    <li class="missdiol-pic9">
+    <li class="missdiol-pic9 ">
       <a href="#">
         <figure>
           <img src="./img/HBJ/Miss^diol/natali^portman.jpg" alt="">
--- js/about-woman.js
+++ js/about-woman.js
@@ -1,3 +1,22 @@
 $(function(){
-  
+    var ceta
+    var cont
+    var conh
+  function pallarex(){
+     cont=$("img").offset().top
+     conh=$("img").innerHeight()
+     ceta=0
+    if(ceta<0){
+      ceta=1
+    }
+    if(ceta<0){
+      ceta=1
+    }
+  }
+  pallarex()
+  $(window).resize(function(){
+    pallarex()
+  }).scroll(function(){
+    pallarex()
+  })
 })
