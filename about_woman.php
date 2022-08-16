5cf82e447bcda120b8e7a94b3bf337dabeba03a8
DESKTOP-SL0JOSE\tj-bu <>
2022-08-16 18:25:24 +0900

    2022-08-16 18:24 텍스트 편집 및 버튼 꾸미기 패럴랙스모션 패치 정형빈

--- about_woman.php
+++ about_woman.php
@@ -1,26 +1,28 @@
 <?php include 'sub-header.php' ?>
 <link rel="stylesheet" href="./css/about-woman-kjh.css">
-<script src="./js/about-woman.js"></script>
-<script src="./js/smoothscroll.js"></script>
 <script src="./js/window.js"></script>
+<script src="./js/smoothscroll.js"></script>
+<script src="./js/about-woman.js"></script>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <section class="about-woman-section about-woman-section1">
   <ul class="clearfix">
     <li class="missdiol-pic1">
       <a href="#">
         <figure>
           <img src="./img/HBJ/Miss^diol/missdiol^eaude^parpum.jpg" alt="">
-        </figure>
+        </figure> 
+      </a>
         <div class="missdiol-textbox">
           <div>
             <h2>"올 뉴 미스디올 오 드 퍼퓸"</h2> <br>
-            <p>싱그러운 은방울 꽃과 선명한 피오니가 장미의 꽃잎처럼 부드럽고 센슈얼틱한 매력을 이끌어내고 <br>
+            <p>싱그러운 은방울 꽃과 선명한 피오니가 장미의 꽃잎처럼 부드럽게 센슈얼틱한 매력을 이끌어내고 
               파우더리스한 아이리스가 향기를 한 층 더 풍성하게 피워냅니다.</p> <br>
-           <button class="more">
-            <div>more</div>
-           </button>
+            <button class="more">
+              <div>more</div>
+            </button>
           </div>
         </div><!-- textbox -->
-      </a>
     </li><!-- 1 -->
     <li class="missdiol-pic2">
       <a href="#">
@@ -43,10 +45,10 @@
       <div class="missdiol-textbox">
 
         <div class="">
-          <h2>" 새로운 미스디올 컬렉션 그 한 병 안에서 내츄럴틱 한 꽃의 향연에 빠지다."</h2>  <br>
-            <p>싱그러운 은방울꽃,선명해지는 피오니,파우더리스한 아이리스,상큼한블러드 오렌지와 고귀한 만다린,베티퍼,패츌리,가이악...그리고 언제나 만나도 새로운 메이로즈까지
-              이 모든 향을  미스디올 컬렉션에 담아냈습니다. 지금 인근 매장에서 만나보세요..!
-            </p>
+          <h2>" 새로운 미스디올 컬렉션 그 한 병 안에서 내츄럴틱 한 꽃의 향연에 빠지다."</h2> <br>
+          <p>싱그러운 은방울꽃,선명해지는 피오니,파우더리스한 아이리스,상큼한블러드 오렌지와 고귀한 만다린,베티퍼,패츌리,가이악...그리고 언제나 만나도 새로운 메이로즈까지
+            이 모든 향을 미스디올 컬렉션에 담아냈습니다. 지금 인근 매장에서 만나보세요..!
+          </p>
         </div>
 
       </div><!-- long textbox -->
@@ -55,9 +57,9 @@
       <div class="missdiol-textbox">
         <div>
 
-                  <h2>2022</h2> <br>
+          <h2>2022</h2> <br>
           <p> FALL INTO BEAUTIFUL SCENT <br></p>
-            <span>www.dior.com</span> 
+          <span>www.dior.com</span>
 
         </div>
       </div><!-- large textbox-->
@@ -86,12 +88,12 @@
       <div class="missdiol-textbox">
         <div>
           <h2>"올 뉴 미스디올 오 드 뚜왈렛"</h2> <br>
-          <p>탑 노트에서 블러드 오렌지와 만다린이 눈부신 매력을 더해주며, <br>
-            미들 노트에서 은방울꽃 어코드가 그라스산 로즈의 향에 가볍고 산뜻한 향기를 더해줍니다. <br>
+          <p>탑 노트에서 블러드 오렌지와 만다린이 눈부신 매력을 더해주며, 
+            미들 노트에서 은방울꽃 어코드가 그라스산 로즈의 향에 가볍고 산뜻한 향기를 더해줍니다.
             베이스 노트의 선명한 패츌리 어코드가 행복으로 가득하게 만들어 줍니다.</p><br>
-            <button class="more">
-              <div>more</div>
-             </button>
+          <button class="more">
+            <div>more</div>
+          </button>
         </div>
       </div>
       <!-- 일반 박스 형태 텍스트박스 -->
@@ -105,10 +107,10 @@
       <div class="missdiol-textbox">
         <div>
           <h2>"올 뉴 미스디올 로즈 에쌍스"</h2><br>
-          <p>그라스의 마농 농장에서 수확한 메이로즈와 싱그럽고 매혹적인 플로럴과 우디의 베티버,패츌리 그리고 가이악 향기까지... <br>
+          <p>그라스의 마농 농장에서 수확한 메이로즈와 싱그럽고 매혹적인 플로럴과 우디의 베티버,패츌리 그리고 가이악 향기까지... 
             조화롭게 어우러진 가운데 머스크 노트가 부드럽게 감싸진 특별한 향기를 선사하는 최초의 미스 디올 빈티지 리미티드 향수입니다.</p><br>
           <!-- 일반 박스 형태 텍스트박스 -->
-          <button class="more" >
+          <button class="more">
             <div> more </div>
           </button>
         </div>
@@ -125,4 +127,180 @@
 
   </ul>
 </section>
+<hr>
+<!-- --------------------------------------------------------------------- -->
+<section class=" about-woman-section about-woman-section2">
+  <ul>
+    <li>
+      <a href="">
+        <figure>
+          <img src="" alt="">
+          <img src="" alt="">
+        </figure>
+      </a>
+      <div class="textbox">
+        <h3>쟈도르 디올...  <br>
+        인근 매장에서 만나보세요!</h3>
+        <p>쟈도르만의 여전사같은 에너제틱함..!</p>
+      </div>
+      <button class="more">
+        <div>more</div>
+      </button>
+    </li><!-- li1 -->
+    <li>
+      <a href="">
+        <figure>
+          <img src="./img/HBJ/ja^dor/ja^dor_model.jpg" alt="">
+          <img src="" alt="">
+        </figure>
+      </a>
+      <div class="textbox">
+        <h3></h3>
+        <p></p>
+      </div>
+      <button class="more">
+        <div>more</div>
+      </button>
+    </li><!-- li2 -->
+    <li>
+      <a href="">
+        <figure>
+          <img src="./img/HBJ/ja^dor/ja^dor^model^background.jpg" alt="">
+          <img src="" alt="">
+        </figure>
+      </a>
+      <div class="textbox">
+        <h3></h3>
+        <p></p>
+      </div>
+      <button class="more">
+        <div>more</div>
+      </button>
+    </li><!-- li3 -->
+    <li>
+      <a href="">
+        <figure>
+          <img src="" alt="">
+          <img src="" alt="">
+        </figure>
+      </a>
+      <div class="textbox">
+        <h3></h3>
+        <p></p>
+      </div>
+      <button class="more">
+        <div>more</div>
+      </button>
+    </li><!-- li4 -->
+    <li>
+      <a href="">
+        <figure>
+          <img src="" alt="">
+          <img src="" alt="">
+        </figure>
+      </a>
+      <div class="textbox">
+        <h3></h3>
+        <p></p>
+      </div>
+      <button class="more">
+        <div>more</div>
+      </button>
+    </li><!-- li5 -->
+    <li>
+      <a href="">
+        <figure>
+          <img src="" alt="">
+          <img src="" alt="">
+        </figure>
+      </a>
+      <div class="textbox">
+        <h3></h3>
+        <p></p>
+      </div>
+      <button class="more">
+        <div>more</div>
+      </button>
+    </li><!-- li6 -->
+    <li>
+      <a href="">
+        <figure>
+          <img src="" alt="">
+          <img src="" alt="">
+        </figure>
+      </a>
+      <div class="textbox">
+        <h3></h3>
+        <p></p>
+      </div>
+      <button class="more">
+        <div>more</div>
+      </button>
+    </li><!-- li7 -->
+    <li>
+      <a href="">
+        <figure>
+          <img src="" alt="">
+          <img src="" alt="">
+        </figure>
+      </a>
+      <div class="textbox">
+        <h3></h3>
+        <p></p>
+      </div>
+      <button class="more">
+        <div>more</div>
+      </button>
+    </li><!-- li8 -->
+    <li>
+      <a href="">
+        <figure>
+          <img src="" alt="">
+          <img src="" alt="">
+        </figure>
+      </a>
+      <div class="textbox">
+        <h3></h3>
+        <p></p>
+      </div>
+      <button class="more">
+        <div>more</div>
+      </button>
+    </li><!-- li9 -->
+    <li>
+      <a href="">
+        <figure>
+          <img src="" alt="">
+          <img src="" alt="">
+        </figure>
+      </a>
+      <div class="textbox">
+        <h3></h3>
+        <p></p>
+      </div>
+      <button class="more">
+        <div>more</div>
+      </button>
+    </li><!-- li10 -->
+    <li>
+      <a href="">
+        <figure>
+          <img src="" alt="">
+          <img src="" alt="">
+        </figure>
+      </a>
+      <div class="textbox">
+        <h3></h3>
+        <p></p>
+      </div>
+      <button class="more">
+        <div>more</div>
+      </button>
+    </li><!-- li11 -->
+  </ul>
+</section>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <?php include 'sub-footer.php' ?>
--- js/about-woman.js
+++ js/about-woman.js
@@ -1,22 +1,23 @@
-$(function(){
-    var ceta
-    var cont
-    var conh
-  function pallarex(){
-     cont=$("img").offset().top
-     conh=$("img").innerHeight()
-     ceta=0
-    if(ceta<0){
-      ceta=1
-    }
-    if(ceta<0){
-      ceta=1
-    }
-  }
-  pallarex()
-  $(window).resize(function(){
+$(function () {
+var cont=$("img").offset().top
+var conh=$("img").innerHeight()
+var ceta
+ function pallarex(){
+$(".about-woman-section1 li").each(function(){
+
+  cont=$(this).offset().top
+  conh=$(this).innerHeight()
+  ceta=1+(scrt-(cont-winh*0.5+conh*0.5))*0.085
+  $(this).find("img").css({"transform":"scale(0.94) translateY("+ceta+"px)"})
+
+})
+
+
+ }//pallarex fn_
+ pallarex()
+  $(window).scroll(function(){
     pallarex()
-  }).scroll(function(){
+  }).resize(function(){
     pallarex()
   })
 })
