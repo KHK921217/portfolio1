6e93a7d297f4994a531774cdda56a40b7ff7f221
DESKTOP-641VCJ4\tj-bu <726ouleesun@naver.com>
2022-08-19 18:16:10 +0900

    9day 08/19 06:15 ydr

--- about_man.php
+++ about_man.php
@@ -10,7 +10,7 @@
 
   <main class="ydr-sauvage-main">
     <ul class="ydr-parallax">
-      <li class="ydr-parallax-li">
+      <li>
         <a href="#">
           <figure>
             <img src="./img/yoodareen/savage/3348901567572_0.jpg" alt="">
@@ -21,7 +21,7 @@
           </figcaption>
         </a>
       </li>
-      <li class="ydr-parallax-li">
+      <li>
         <a href="#">
           <figure>
             <img src="./img/yoodareen/savage/product-2_1440_1200_500x500_crop_center.jpg" alt="">
@@ -32,7 +32,7 @@
           </figcaption>
         </a>
       </li>
-      <li>
+      <li class="ydr-lemon-img">
         <img class="ydr-lemon" src="./img/yoodareen/savage/edt-1_1440_1200_500x_crop_center.jpg" alt="">
       </li>
     </ul>
@@ -99,21 +99,23 @@
 
 
 <section class="ydr-homme">
+  <div>
   <figure>
-    <img class="ydr-figure-img1" src="./img/yoodareen/homme/91e91562d5295933c89ec3649e8045eb.jpg" alt="">
-    <figcaption class="ydr-home-text">
-      <h3><img src="./img/yoodareen/homme/diorhomme-text.png" alt=""></h3>
-      <p>
-        디올 옴므는 가공되지 않은 우드와 다채로운 부드러움으로 관능적인 남성성을 새롭게 재해석합니다. <br>
-        부드러운 우드 노트의 중심에는, 남성적인 아틀라스 시더가 스파이스 향이 감도는 아이티산 베티버의 따뜻함을 감싸줍니다. 마치 다듬어 지지 않은 목재가 디올에 의해 수작업으로 정교히 조각된 듯한 느낌을
-        선사합니다.
-        디올 옴므는 현대 남성의 다채로운 모습을 담아내었습니다. 남성미가 넘치는 시그니처 향뿐만 아니라 강인함과 부드러움이 교차하는 부드럽고 센슈얼한 느낌도 함께 선사합니다. <br>
-        디올 옴므 오 드 뚜왈렛은 대담한 향기를 지닌 향수로 관능적인 인상을 남깁니다. <br>
-      </p>
-    </figcaption>
+     <img class="ydr-figure-img" src="./img/yoodareen/homme/91e91562d5295933c89ec3649e8045eb.jpg" alt="">  
   </figure>
+  <figcaption class="ydr-home-text">
+    <h3><img src="./img/yoodareen/homme/diorhomme-text.png" alt=""></h3>
+    <p>
+      디올 옴므는 가공되지 않은 우드와 다채로운 부드러움으로 관능적인 남성성을 새롭게 재해석합니다. <br>
+      부드러운 우드 노트의 중심에는, 남성적인 아틀라스 시더가 스파이스 향이 감도는 아이티산 베티버의 따뜻함을 감싸줍니다. 마치 다듬어 지지 않은 목재가 디올에 의해 수작업으로 정교히 조각된 듯한 느낌을
+      선사합니다.
+      디올 옴므는 현대 남성의 다채로운 모습을 담아내었습니다. 남성미가 넘치는 시그니처 향뿐만 아니라 강인함과 부드러움이 교차하는 부드럽고 센슈얼한 느낌도 함께 선사합니다. <br>
+      디올 옴므 오 드 뚜왈렛은 대담한 향기를 지닌 향수로 관능적인 인상을 남깁니다. <br>
+    </p> 
+  </figcaption>
+    </div>
 
-  <nav class="ydr-homme-nav">
+<!--   <nav class="ydr-homme-nav">
     <div class="ydr-homme-name">
       <span class="ydr-button">
         <button data-n="1">DIOR HOMME COLOGNE</button>
@@ -122,7 +124,7 @@
       </span>
       <p>디올 옴므는 마치 우아하고 모던한 수트와 같이 향이 퍼지는 곳마다 우아함을 선사합니다. 디올 옴므 인텐스 오 드 퍼퓸은 극도로 장엄한 세련미를 대담하고 강렬하게 표출합니다. 감각적인 엠버 노트와 고귀한 우드 베이스로 강조된 인텐스 아이리스는 강렬한 매력을 선사합니다. 향기롭고 관능적인 향기가 끊임없이 이어집니다.</p>
     </div>
-    <img class="ydr-figure-img2" src="./img/yoodareen/homme/3348901419147_2.jpg" alt="">
+    <img class="ydr-figure-img" src="./img/yoodareen/homme/3348901419147_2.jpg" alt="">
   </nav>
 
   <section class="ydr-homme-purfume">
@@ -131,8 +133,8 @@
       <li><img src="./img/yoodareen/homme/3348901419147_0.jpg" alt=""></li>
       <li><img src="./img/yoodareen/homme/3348900838185_0.jpg" alt=""></li>
     </ul>
-  </section>
-</section>
+  </section>-->
+</section> 
 <svg>
   <filter id="filter">
     <feTurbulence id="water" numOctaves="5" seed="3" baseFrequency="0.02 0.5" />
--- js/about_man_script.js
+++ js/about_man_script.js
@@ -1,49 +1,35 @@
-$(function(){
-
-  var boxt
-  var boxh
-  var metaY
- 
-  function parallax(){
-    $('.ydr-parallax-li').each(function(){
-      boxt = $(this).offset().top
-      boxh = $(this).innerHeight()
-      metaScale = 1 + Math.abs(scrt - (boxt - winh*0.5 + boxh*0.5)) * -0.0008
-      if(metaScale < 0){metaScale = 0}
-      $(this).children('a').css({'transform':'scale('+metaScale+')'})
-      metaY = 0 + (scrt - (boxt - winh*0.5 + boxh*0.5)) * 0.2
-      $(this).find('img').css({'transform':'translateY('+metaY+'px) scale(1)'})
-
-      if(scrt >= boxt - winh * 0.8){
-        $(this).addClass('active')
-      }else{
-        $(this).removeClass('active')
-      }
-    })//each
-
-    boxt = $('.ydr-perfum').offset().top
-    boxh = $('.ydr-perfum').innerHeight()
-    meta = 1 + (scrt - (boxt - winh*0.5 + boxh*0.5)) * 0.01
-    $('.ydr-perfum').css({'opacity':meta})
-
-    boxt = $('.ydr-figure-img1').offset().top
-    boxh = $('.ydr-figure-img1').innerHeight()
-    meta = 1 + (scrt - (boxt - winh*0.5 + boxh*0.5)) * 0.01
-    $('.ydr-figure-img1').css({'opacity':meta})
-
-    boxt = $('.ydr-figure-img2').offset().top
-    boxh = $('.ydr-figure-img2').innerHeight()
-    meta = 1 + (scrt - (boxt - winh*0.5 + boxh*0.5)) * 0.01
-    $('.ydr-figure-img2').css({'opacity':meta})
+$(function () {
+     var boxt
+    var boxh  
+    var metaScale
+    var meta
+    var metaY
+    function parallax(){
+    
+      boxt = $('.ydr-perfum').offset().top
+      boxh = $('.ydr-perfum').innerHeight()
+      meta = 1 + (scrt - (boxt - winh*0.5 + boxh*0.5)) * 0.01
+      $('.ydr-perfum').css({'opacity':meta})
 
 
-  }//fn
+   /*   $('.ydr-homme div').each(function(){
+        boxt = $(this).offset().top
+        boxh = $(this).innerHeight()
+        metaScale = 1 + Math.abs(scrt - (boxt - winh*0.5 + boxh*0.5)) * -0.002
+        if(metaScale < 0){metaScale = 0}
+        $(this).children('a').css({'transform':'scale('+metaScale+')'})
+        metaY = 0 + (scrt - (boxt - winh*0.5 + boxh*0.5)) * 0.15
+        $(this).find('img').css({'transform':'translateY('+metaY+'px) scale(1.1)'})
+   
+      })//each */
 
-  parallax()
-  $(window).scroll(function(){
+    }//fn
+  
     parallax()
-  }).resize(function(){
-    parallax()
-  })
+    $(window).scroll(function(){
+      parallax()
+    }).resize(function(){
+      parallax()
+    }) 
 
 })//ready
