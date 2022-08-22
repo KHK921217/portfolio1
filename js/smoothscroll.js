ac1ba849060d430915985855e20a6fd9f80eadde
DESKTOP-0ABCL26\tj-bu <ljm5704@gmail.com>
2022-08-22 11:17:45 +0900

    smoothscroll.js 파일 소스 수정_김희경

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -1,4 +1,10 @@
-$(function(){
+$(function(){ 
+  var isMobile = false; 
+  var filter = "win16|win32|win64|mac";
+  if (navigator.platform) {
+    isMobile = filter.indexOf(navigator.platform.toLowerCase()) < 0;
+  }
+  if(isMobile===false){
   var speed = 0
   var delta
 
@@ -14,6 +20,6 @@ $(function(){
       top: scrt + speed,
     })
     speed = speed * 0.9/* 1에 가까운 수일수록 느리게 감 */
-  },20)
+  },20)}
 
 })//ready
