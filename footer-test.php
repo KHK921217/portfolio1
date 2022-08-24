9d1e0e181ae832a6c1ba8c502eaedff624d4242f
DESKTOP-ST2AIC0\tj-bu <tjdwlsakfn2@naver.com>
2022-08-24 18:21:11 +0900

    (이성진)8/24일 푸터 반응형 디자인 조정

--- footer-test.php
+++ footer-test.php
@@ -1,62 +1,95 @@
-<div class="lee-footer">
-<div class="lee-footer-main">
-  <ul>
-    <li>
-      <h6>
-        <a href="#">Delivery and returns</a>
-        <a href="#">고객센터</a>
-        <a href="#">FAQ</a>
-        <a href="#">사이트맵</a>
-      </h6>
-    </li>
-    <li>
-      <h6>
-        <b>부띠크 찾기</b>
-      </h6>
-      <h6>
-        <i><a href="#">Christian Dior Couture</a></i>
-        <i><a href="#">Purfurms Christian Dior</a></i>
-      </h6>
-    </li>
-  </ul>
-
-  <section class="lee-section1-1">
-  <p>
-    크리스챤디올꾸뛰르코리아주식회사 | 04539 서울특별시 중구 을지로5길 19, 26층(수하동) | 사업자등록번호: 120-81-74197<br>
-    대표자: 트렁히엔트란, Khong May Wan Sharon | 통신판매업 신고번호: 2021-서울중구-01116 | 사업자정보확인<br>
-    개인정보관리책임자: 김지혜I 고객센터: 02-3480-0104 (contactdiorkr@dior.com) | 호스팅 서비스: Smile Hosting<br>
-    COPYRIGHT © CHRISTIAN DIOR COUTURE KOREA ALL RIGHTS RESERVED.
-    이니시스 구매안전 서비스 가입함
-  </p>
-  </section>
-
-  <section class="lee-section1-2">
-  <p>크리스챤디올꾸뛰르코리아주식회사 | 04539 서울특별시 중구 을지로5길</p>
-  <p>19, 26층(수하동) | 사업자등록번호: 120-81-74197</p>
-  <p>대표자: 트렁히엔트란, Khong May Wan Sharon</p>
-  <p>| 통신판매업 신고번호: 2021-서울중구-01116 | 사업자정보확인</p>
-  <p>개인정보관리책임자: 김지혜I 고객센터: 02-3480-0104 (contactdiorkr@dior.com)</p>
-  <p>| 호스팅 서비스: Smile Hosting</p>
-  <p>COPYRIGHT © CHRISTIAN DIOR COUTURE KOREA ALL RIGHTS RESERVED.</p>
-  <p>이니시스 구매안전 서비스 가입함</p>
-  </section>
-
-  <button class="lee-footer-bottom">
-    <a href=""><i class="fa-brands fa-square-instagram"></i></a>
-    <a href=""><i class="fa-brands fa-facebook"></i></a>
-    <a href=""><i class="fa-brands fa-square-twitter"></i></a>
-  </button>
-</div>
 <div class="lee-footer-aside">
-  <button>
-    <select name="mh_type" id="mh_counsel" required>
-      <option value="country" selected>국가</option>
-    </select>
-    <select name="mh_type" id="mh_counsel" required>
-      <option value="language" selected>언어</option>
-    </select>
-  </button>
-</div>
+
+  </div>
+<div class="lee-footer">
+  <div class="lee-footer-main">
+    <section class="lee-section1-1">
+      <ul>
+        <li>
+          <h6>
+            <a href="#">Delivery and returns</a>
+            <a href="#">고객센터</a>
+            <a href="#">FAQ</a>
+            <a href="#">사이트맵</a>
+          </h6>
+        </li>
+        <li>
+          <h6>
+            <b>부띠크 찾기</b>
+          </h6>
+          <h6>
+            <i><a href="#">Christian Dior Couture</a></i>
+            <i><a href="#">Purfurms Christian Dior</a></i>
+          </h6>
+        </li>
+      </ul>
+      <p>
+        크리스챤디올꾸뛰르코리아주식회사 | 04539 서울특별시 중구 을지로5길 19, 26층(수하동) | 사업자등록번호: 120-81-74197<br>
+        대표자: 트렁히엔트란, Khong May Wan Sharon | 통신판매업 신고번호: 2021-서울중구-01116 | 사업자정보확인<br>
+        개인정보관리책임자: 김지혜I 고객센터: 02-3480-0104 (contactdiorkr@dior.com) | 호스팅 서비스: Smile Hosting<br>
+        COPYRIGHT © CHRISTIAN DIOR COUTURE KOREA ALL RIGHTS RESERVED.
+        이니시스 구매안전 서비스 가입함
+      </p>
+    </section>
+
+    <section class="lee-section1-2">
+      <ul>
+        <li>
+          <h6>
+            <b>부띠크 찾기</b>
+          </h6>
+          <h6>
+            <i><a href="#">Christian Dior Couture</a></i>
+            <i><a href="#">Purfurms Christian Dior</a></i>
+          </h6>
+        </li>
+      </ul>
+      <p>크리스챤디올꾸뛰르코리아주식회사 | 04539 서울특별시 중구 을지로5길</p>
+      <p>19, 26층(수하동) | 사업자등록번호: 120-81-74197</p>
+      <p>대표자: 트렁히엔트란, Khong May Wan Sharon</p>
+      <p>| 통신판매업 신고번호: 2021-서울중구-01116 | 사업자정보확인</p>
+      <p>개인정보관리책임자: 김지혜I 고객센터: 02-3480-0104 (contactdiorkr@dior.com)</p>
+      <p>| 호스팅 서비스: Smile Hosting</p>
+      <p>COPYRIGHT © CHRISTIAN DIOR COUTURE KOREA ALL RIGHTS RESERVED.</p>
+      <p>이니시스 구매안전 서비스 가입함</p>
+      <ul>
+        <li>
+          <h6>
+            <a href="#">Delivery and returns</a>
+            <a href="#">고객센터</a>
+            <a href="#">FAQ</a>
+            <a href="#">사이트맵</a>
+          </h6>
+        </li>
+      </ul>
+    </section>
+
+    <button class="lee-footer-bottom">
+      <a href=""><i class="fa-brands fa-square-instagram"></i></a>
+      <a href=""><i class="fa-brands fa-facebook"></i></a>
+      <a href=""><i class="fa-brands fa-square-twitter"></i></a>
+    </button>
+  </div>
+  <div class="lee-footer-aside2">
+    <ul>
+    <li>Country: </li>
+    <button>
+      <select name="mh_type" id="mh_counsel" required>
+        <option value="country" selected>한국</option>
+        <option value="country" selected>미국</option>
+      </select><br>
+    </button>
+    </ul>
+    <ul>
+    <li>language: </li>
+    <button>
+      <select name="mh_type" id="mh_counsel" required>
+        <option value="language" selected>한국어</option>
+        <option value="language" selected>영어</option>
+      </select>
+    </button>
+    </ul>
+  </div>
 </div>
 </body>
 
