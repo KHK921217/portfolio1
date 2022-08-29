a25675b8f56a5d4b5274f544de04e2b4d1ba8e98
DESKTOP-0ABCL26\tj-bu <ljm5704@gmail.com>
2022-08-29 18:21:29 +0900

    김희경 about dior_구조 변경

--- about_dior.php
+++ about_dior.php
@@ -2,103 +2,80 @@
 <link rel="stylesheet" href="./css/about_dior.css"> 
 <script src="./js/about_dior.js"></script> 
 
+  <div class="kh-width">
     <div class="kh-title">
       <img src="./img/khk/brand/title1.png" alt="title">
     </div><!-- 제목 -->
 
     <section class="kh-brand-section1" data-n="1">
-      <figure>
-        <img src="./img/khk/brand/dior123 copy.png" alt="img1">
-      </figure>
-      
+      <div class=kh-left-box>
+        <figure class="kh-left-img">
+          <img src="./img/khk/brand/point1.png" alt="img1">
+        </figure>
+        <div class="kh-left-txt">
+          <div></div>
+          <h4>제조이력 준수</h4>
+          <p>모든 제품은 엄격한 규정을 준수하며 제품 품질, 원재료 제조이력에 대한 DIOR의 자체기준을 준수합니다.</p>
+        </div>
+      </div>
     </section><!-- section1-->
 
     <section class="kh-brand-section2" data-n="2">
-      <figure>
-        <img src="./img/khk/brand/brand.png" alt="img2">
-        <figcaption>
-          <p>모든 제품은 엄격한 규정을 준수하며 제품 품질, 원재료 제조이력에 대한 DIOR의 자체기준을 준수합니다. </p>
-        </figcaption>
-      </figure>
+      <div class=kh-right-box>
+        <figure class="kh-right-img">
+          <img src="./img/khk/brand/point2.png" alt="img2">
+        </figure>
+        <div class="kh-right-txt">
+          <div></div>
+          <h4>철저한 IN-VITRO 평가</h4>
+          <p>'제품이 본연의 상태를 오래도록 유지할 수 있도록 성분 및 포뮬러에 대한 IN-VITRO 및 IN-VIVO를 평가합니다.'</p>
+        </div>
+      </div>
     </section><!-- section2-->
 
     <section class="kh-brand-section3" data-n="3">
-      <figure>
-        <img src="./img/khk/brand/brand1.png" alt="img3">
-        <figcaption>
-          <p>'제품이 본연의 상태를 오래도록 유지할 수 있도록 성분 및
-            포뮬러에 대한 IN-VITRO 및 IN-VIVO를 평가합니다.'
-          </p>
-        </figcaption>
-      </figure>
+      <div class=kh-left-box2>
+        <figure class="kh-left-img">
+          <img src="./img/khk/brand/point3.png" alt="img3">
+        </figure>
+        <div class="kh-left-txt">
+          <div></div>
+          <h4>지속가능한 개발</h4>
+          <p>'DIOR은 지속 가능한 뷰티를 위해 환경을 고려한 성분에 집중해야한다고 믿습니다.'</p>
+        </div>
+      </div>
     </section><!-- section3-->
 
     <section class="kh-brand-section4" data-n="4">
-      <div class='kh-sec4-container'>
-        <div class="kh-div1">
-          <figure>
-            <img src="./img/khk/brand/brand3.png" alt="img4">
-          </figure>
-          <figcaption>
-            <p>DIOR은 지속 가능한 뷰티를 위해 환경을 고려한 성분에 집중해야한다고 믿습니다.</p>
-          </figcaption>
-        </div>
-        <div class="kh-div2">
-          <figure>
-            <div><img src="./img/khk/brand/dior12345.png" alt="img5"></div>
-            <figcaption>
-              <p>DIOR은 생물의 다양성 및 사회에 더 나은 영향을 주기 위해 지속적으로 노력합니다.</p>
-            </figcaption>
-          </figure>
-        </div>
         <div class="kh-up_txt">
-      <h4>SPEACIAL BENEFIT TO MEMBER!</h4>
-      <p>우수한 품질을 바탕으로 까다로운 소비자들을 만족시키고 유통 채널의 다각화와 전문화로
+          <h4>SPEACIAL BENEFIT TO MEMBER!</h4>
+          <p>우수한 품질을 바탕으로 까다로운 소비자들을 만족시키고 유통 채널의 다각화와 전문화로
   피부타입은 개인의 관리나 계절의 변화에 따라 달라질 수 있으므로 아름다움과 건강함을 지켜보세요.</p>
-      <div class="kh-point">
-        <ul>
-          <li>
-            <figure>
-              <img src="./img/khk/brand/point1.png" alt="point_img1">
-            </figure>
-              <h2>New member</h2>
-          </li>
-          <li>
-            <figure>
-              <img src="./img/khk/brand/point2.png" alt="point_img2">
-            </figure>
-            <h2>Photo Review</h2>
-          </li>
-          <li>
-            <figure><img src="./img/khk/brand/point3.png" alt="point_img3"></figur>
-            <h2>Event</h2>
-          </li>
-        </ul>
-      </div><!-- kh-point -->
-    </div><!-- kh-up_txt -->
-      </div><!-- sec4 container -->
-  </section><!-- section4-->
+          <div class="kh-point">
+            <ul>
+              
+              <li>
+              <i class="fa-brands fa-ember"></i>
+                  <h2>New member</h2>
+                  <p>신규 회원가입시 웰컴쿠폰 증정!</p>
+              </li>
+              
+              <li>
+              <i class="fa-solid fa-images"></i>
+                <h2>Photo Review</h2>
+                <p>포토리뷰 올리면 포인트가 2배!</p>
+              </li>
+             
+              <li>
+              <i class="fa-solid fa-gift"></i>
+                <h2>Event</h2>
+                <p>이달의 특별한 이벤트!</p>
+              </li>
+            </ul>
 
+          </div><!-- kh-point -->
+        </div><!-- kh-up_txt -->
+   </section><!-- section4-->
+  </div>
 
-
-    <div class="kh-scr-container">
-      <i class="fa-solid fa-computer-mouse mouse">
-        <p>scroll</p>
-      </i>
-      <nav class="kh-scrollspy">
-        <button class="kh-btn1" data-n="1"></button>
-        <button class="kh-btn2" data-n="2"></button>
-        <button class="kh-btn3" data-n="3"></button>
-        <button class="kh-btn4" data-n="4"></button>
-      </nav>
-    </div><!-- kh-scr-container -->
-
-  <svg>
-    <filter id="filter">
-    <feTurbulence id="water" numOctaves="3" seed="1" baseFrequency="0.02 0.5" />
-    <feDisplacementMap scale="10" in="SourceGraphic" />
-    <animate href="#water" attributeName="baseFrequency" keyTimes="0;0.5;1"
-    values="0.002;0.008;0.002" dur="10s" repeatCount="indefinite"/>
-    </filter>
-  </svg>
 <?php include 'sub-footer.php' ?>
