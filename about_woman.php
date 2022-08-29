1cc130ed0fc2c0644e38fbe70a92cecde8ad7d7f
DESKTOP-SL0JOSE\tj-bu <>
2022-08-29 18:18:41 +0900

    2022-08-29 18:18  이미지 삽입 및 텍스트 위치 수정 정형빈

--- about_woman.php
+++ about_woman.php
@@ -14,51 +14,52 @@
       <img src="./img/woman/Miss^diol/missdiol^flower.jpg" alt="">
     </figure>
     <figure class="imgbox3">
-      <img src="./img/woman/m3.jpg" alt="">
+      <img src="./img/woman/woman-page_top-slider/ja^dor_model2_2480.3216.png" alt="">
     </figure>
     <figure class="imgbox4">
-      <img src="./img/woman/m4.jpg" alt="">
+      <img src="./img/woman/woman-page_top-slider/ja^dor-scaleshot.remake_2480.3216.jpg" alt="">
     </figure>
     <figure class="imgbox5">
-      <img src="./img/woman/m5.jpg" alt="">
+      <img src="./img/woman/woman-page_top-slider/joy-by-diol_intence_1500.jpg" alt="">
     </figure>
     <figure class="imgbox6">
-      <img src="./img/woman/m6.jpg" alt="">
+      <img src="./img/woman/woman-page_top-slider/joy-by-diol_intence-flower.jpg" alt="">
     </figure>
     <figure class="imgbox7">
-      <img src="./img/woman/m7.jpg" alt="">
+      <img src="./img/woman/woman-page_top-slider/new-ja^dor-in_nature-1050x1400.jpg" alt="">
     </figure>
     <figure class="imgbox8">
-      <img src="./img/woman/m8.jpg" alt="">
+      <img src="./img/woman/woman-page_top-slider/new-ja^dor_model3.jpg" alt="">
     </figure>
   </div><!-- img_container -->
   <h2 class="type1">Woman's Perfume</h2>
   <div class="textbox_container type2">
     <div class="textbox textbox1">
       <h3>
-        <b>ZIP-UP JACKET</b>
-        <span>Zip-Up Jacket in black and white cotton poplin</span>
+        <b>Miss Dior</b>
+        <span>WAKE UP FOR LOVE</span>
       </h3>
       <a href="#" class="more">MORE <i class="far fa-arrow-alt-circle-right"></i></a>
     </div>
     <div class="textbox textbox2">
       <h3>
-        <b>WINDBREAKER</b>
-        <span>Tracksuit nylon Jacket with Balenciaga logo</span>
+        <b>j'a doré</b>
+        <span>
+          all women and their daring...elegance and Femininity j'adoré praising meet.</span>
       </h3>
       <a href="#" class="more">MORE <i class="far fa-arrow-alt-circle-right"></i></a>
     </div>
     <div class="textbox textbox3">
       <h3>
-        <b>DÉFILÉ XL T-SHIRT</b>
-        <span>Défilé XL T-shirt in black vintage jersey</span>
+        <b>JOY BY Dior</b>
+        <span>The fascinating fragrance is the same as a poem that sings the joy of life.</span>
       </h3>
       <a href="#" class="more">MORE <i class="far fa-arrow-alt-circle-right"></i></a>
     </div>
     <div class="textbox textbox4">
       <h3>
-        <b>DENIM JACKET</b>
-        <span>denim jacket with Balenciaga logo printed at back</span>
+        <b>J'ADORE PARFUM D'EAU</b>
+        <span>New fragrance,New fascinate</span>
       </h3>
       <a href="#" class="more">MORE <i class="far fa-arrow-alt-circle-right"></i></a>
     </div>
@@ -83,19 +84,19 @@
             c0,0-53.167-0.334-79.209,64.335" />
     </svg>
 
-    <h2 class="bold">Perfume1</h2>
+    <h2 class="bold">Best Line Up</h2>
     <ul class="clearfix">
       <li>
         <div class="inner">
           <figure>
-            <img src="./img/woman/coat.jpg" alt="">
+            <img src="./img/woman/woman-page-road/new-ja^dor-in_nature-933x1400.jpg" alt="">
           </figure>
           <div class="textbox">
-            <h3 class="bold">COAT</h3>
+            <h3 class="bold">새롭게 선보이는 쟈도르 퍼퓸 도</h3>
             <p>
-              Take your tailored style up a notch with this long, lean topcoat in a wool-enriched suiting blend patterned with rich navy checks. Seamless sides lend a look of clean, modern polish.
+            디올이 플로럴 추출물을 풍성하게 담은 워터 베이스 향수를 새롭게 선보입니다. 오랜 시간, 자연스럽게 향수 본연의 강렬하고 풍부한 향기를 경험할 수 있습니다.
             </p>
-            <a href="https://www.balenciaga.com/us/men/coats">View More</a>
+            <a href="https://shop.dior.co.kr/pages/jad-parfum-d-eau-edp-launch-page">더 보기</a>
           </div>
         </div>
       </li>
@@ -103,14 +104,13 @@
       <li>
         <div class="inner">
           <figure>
-            <img src="./img/woman/jacket.jpg" alt="">
+            <img src="./img/woman/woman-page-road/missdiol-y_a_kim3.jpg" alt="">
           </figure>
           <div class="textbox">
-            <h3 class="bold">JACKET</h3>
-            <p>
-              Styled with distressed edges, this trucker jacket features a naturally worn-in look and is punctuated with stamped logo letters across the front.
+            <h3 class="bold">미스디올 오드 퍼퓸</h3>
+            <p>미스디올 오드 퍼퓸은 싱그러운 은방울 꽃과 선명한 피오니가 장미의 꽃잎처럼 부드럽고 센슈얼틱한 매력을 이끌어 내고 파우더리한 아이리스가 향기를 한층 더 풍성하게 피워냅니다
             </p>
-            <a href="https://www.balenciaga.com/us/men/jackets">View More</a>
+            <a href="https://shop.dior.co.kr/pages/discover-miss-dior-eau-de-parfum">더 보기</a>
           </div>
         </div>
       </li>
@@ -118,14 +118,14 @@
       <li>
         <div class="inner">
           <figure>
-            <img src="./img/woman/shirt.jpg" alt="">
+            <img src="./img/woman/woman-page-road/ja^dor.writegold-933x1400.jpg" alt="">
           </figure>
           <div class="textbox">
-            <h3 class="bold">SHIRT</h3>
+            <h3 class="bold">쟈도르 로르</h3>
             <p>
-              When it comes to Balenciaga, unexpected is expected. Helmed by Vetements’ Demna Gvasalia, <br> the brand reassesses the norm and pushes the boundaries of traditional tailoring.
+            쟈도르 로르는 모든 면에서 강렬함을 보여주는 향수입니다. 진귀한 플로랄 앱솔루트가 농축되어 관능적이고 깊은 풍성한 부케 향이 매혹적인 오리엔탈 노트를 즉각적으로 느껴지게 합니다. 
             </p>
-            <a href="https://www.balenciaga.com/us/men/shirts">View More</a>
+            <a href="https://shop.dior.co.kr/pages/womens-fragrance-jadore">더 보기</a>
           </div>
         </div>
       </li>
@@ -133,14 +133,14 @@
       <li>
         <div class="inner">
           <figure>
-            <img src="./img/woman/knit.jpg" alt="">
+            <img src="./img/woman/woman-page-road/joy-by-diol_800x1077.jpg" alt="">
           </figure>
           <div class="textbox">
-            <h3 class="bold">KNITWEAR</h3>
+            <h3 class="bold">조이바이디올 오드 퍼퓸</h3>
             <p>
-              Balenciaga's chunky sweater has been made in Italy from soft yarns that are cable-knitted for nice texture.
+            보틀에 담긴 순수한 환희의 순간. 눈 부신 미소와 같은 조이 바이 디올 오 드 퍼퓸은 풍부한 과즙과 생기 넘치는 탑 노트 뒤로 플라워와 머스크의 하트 노트가 부드러운 손길처럼 감싸안듯 다가옵니다.
             </p>
-            <a href="https://www.balenciaga.com/us/men/knitwear">View More</a>
+            <a href="https://shop.dior.co.kr/pages/womens-fragrance-joy-by-dior">더 보기</a>
           </div>
         </div>
       </li>
@@ -154,73 +154,73 @@
     <ul class="grid">
       <li class="box1">
         <figure>
-          <img src="./img/woman/1-1.jpg" alt="">
+          <img src="./img/woman/woman-page_bottomgrid/new-ja^dor_flower-1050x1400.jpg" alt="">
         </figure>
         <div class="text text1">
-          <p>This Balenciaga's checked jacket <br class="sm"> doubles up as a picnic blanket <br class="sm"> and is a necessity for a weekend <br class="sm"> jaunt to the country.</p>
+          <p>쟈도르의 화이트 플라워 부케가 워터 베이스 포뮬러와 만나 새로운 향기가 탄생하다..!</p>
         </div>
       </li>
       <li class="box2">
         <figure>
-          <img src="./img/woman/1-2.jpg" alt="">
+          <img src="./img/woman/woman-page_bottomgrid/new-ja^dor-bottle-1050.1400.jpg" alt="">
         </figure>
         <div class="text text2">
-          <p class="bold">TWEED COAT</p>
+          <p class="bold">NEW j'a doré</p>
         </div>
       </li>
       <li class="box3">
         <div class="inner">
           <figure>
-            <img src="./img/woman/2-1.jpg" alt="">
+            <img src="./img/woman/woman-page_bottomgrid/missdiol_jisoo_whitepink3-1.jpg" alt="">
           </figure>
           <div class="text text3">
-            <p class="bold">BALENCIAGA</p>
+            <p class="bold">Christian</p>
           </div>
         </div>
       </li>
       <li class="box4">
         <div class="inner">
           <figure>
-            <img src="./img/woman/2-2.jpg" alt="">
+            <img src="./img/woman/woman-page_bottomgrid/missdiol-eau_de_parfum3-2.jpg" alt="">
           </figure>
           <div class="text text4">
-            <p class="bold">CREW</p>
+            <p class="bold">Diór</p>
           </div>
         </div>
       </li>
       <li class="box5">
         <figure>
-          <img src="./img/woman/2-3.jpg" alt="">
+          <img src="./img/woman/woman-page_bottomgrid/missdiol-eau_de_parfum3.jpg" alt="">
         </figure>
       </li>
       <li class="box6">
         <figure>
-          <img src="./img/woman/3-1.jpg" alt="">
+          <img style="filter:grayscale(70%)" src="./img/woman/woman-page_bottomgrid/ja^dor-biter_orangetree.jpg" alt="">
         </figure>
         <div class="text text6">
-          <p class="bold">DOUBLE B<br class="">RIBBED KNIT</p>
+          <p class="bold">ja'doré<br class="">L'Or</p>
         </div>
       </li>
       <li class="box7">
         <figure>
-          <img src="./img/woman/3-2.jpg" alt="">
+          <img style="filter:grayscale(40%)" src="./img/woman/woman-page_bottomgrid/ja^dor-lo.le-vertical.ver.jpg" alt="">
         </figure>
       </li>
       <li class="box8">
         <figure>
-          <img src="./img/woman/4-1.jpg" alt="">
+          <img  src="./img/woman/woman-page_bottomgrid/joy-by-diol-1080x1080 grey.ver.jpg" alt="">
         </figure>
         <div class="text text8">
-          <p class="bold">SOCCER</p>
-          <p class="bold">JERSEY</p>
+          <p class="bold">Joyby</p>
+          <p class="bold">Diór</p>
         </div>
       </li>
       <li class="box9">
         <figure>
-          <img src="./img/woman/4-2.jpg" alt="">
+          <img src="./img/woman/woman-page_bottomgrid/joy-by-diol_contact-ih.cho.jpg" alt="">
         </figure>
         <div class="text text9">
-          <p>Did Demna Gvasalia score <br class="sm"> or should we boot it?</p>
+          <p> <br class="sm"> 어둠 속 어렴풋이 빛이 보이는 기쁨...<br>그 기쁨의 순간을 이 한병에 담아냈습니다.</p>
         </div>
       </li>
     </ul>
